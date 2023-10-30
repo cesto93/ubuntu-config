cf1e8df86e86a906cc532b9f3681acb7d5e04d48
Pier Francesco Contino (EXT Reply) <p.contino@reply.it>
2023-10-30 14:20:40 +0100

    neoutest: add test shortcut

--- after/plugin/neotest.lua
+++ after/plugin/neotest.lua
@@ -6,5 +6,9 @@ require("neotest").setup({
 })
 
 vim.keymap.set("n", "<leader>tl", function() require("neotest").summary.toggle() end)
+vim.keymap.set("n", "<leader>tr", function() require("neotest").run.run() end)
 vim.keymap.set("n", "<leader>ta", function() require("neotest").run.run({ suite = true }) end)
 vim.keymap.set("n", "<leader>tc", function() require("neotest").run.run({ suite = true, extra_args = {"-coverprofile='cover.out'"} }) end)
+vim.keymap.set("n", "<leader>td", function() require("neotest").diagnostic() end)
+vim.keymap.set("n", "<leader>to", function() require("neotest").output_panel.toggle() end)
+
