367b23de9505ae5becc7dea887995144c263150c
Pier Francesco Contino (EXT Reply) <p.contino@reply.it>
2023-11-24 15:04:53 +0100

    neotest: add tv for verbose tests

--- after/plugin/neotest.lua
+++ after/plugin/neotest.lua
@@ -7,6 +7,7 @@ require("neotest").setup({
 
 vim.keymap.set("n", "<leader>tl", function() require("neotest").summary.toggle() end)
 vim.keymap.set("n", "<leader>tr", function() require("neotest").run.run() end)
+vim.keymap.set("n", "<leader>tv", function() require("neotest").run.run({ extra_args = {"-v"} }) end)
 vim.keymap.set("n", "<leader>ta", function() require("neotest").run.run({ suite = true }) end)
 vim.keymap.set("n", "<leader>tc", function() require("neotest").run.run({ suite = true, extra_args = {"-coverprofile='cover.out'"} }) end)
 vim.keymap.set("n", "<leader>td", function() require("neotest").diagnostic() end)
