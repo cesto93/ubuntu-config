6f732a8a5dfa5ea77e873b0df4ff237fa2dfe545
Pier Francesco Contino <pfcontino@gmail.com>
2024-08-22 19:26:52 +0200

    feat(nvim): add gp

---
+++ lua/pier/plugins/gp.lua
@@ -0,0 +1,30 @@
+return {
+	"robitx/gp.nvim",
+	config = function()
+		local conf = {
+			providers = {
+				ollama = { 
+					disable = false, 
+					endpoint = "http://localhost:11434/v1/chat/completions", 
+				},
+				openai = { 
+					disable = true, 
+				},
+			},
+			agents = {
+				{
+					provider = "ollama",
+					name = "OllamaGemma",
+					chat = true,
+					command = true,
+					model = {
+						model = "gemma2:2b",
+					},
+					system_prompt = "You are a general AI assistant.",
+				},
+			},
+			default_chat_agent = "OllamaGemma",
+		}
+		require("gp").setup(conf)
+	end,
+}
