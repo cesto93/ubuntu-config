d22b3638bc5f7b6be6ed6c5ed84b198568babbd7
Pier Francesco Contino (EXT Reply) <p.contino@reply.it>
2024-10-21 11:00:05 +0200

    feat(nvim): add remap comments

--- lua/pier/remap.lua
+++ lua/pier/remap.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = " "
+
+-- Go to directory
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -10,23 +12,32 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
+-- Paste without updating the buffer
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- Delete without updating the buffer
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
--- vim.keymap.set("i", "<C-c>", "<Esc>")
+-- yank to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
+
+-- format the file
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- navigate diagnostics
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace current word on file
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Add executable permission to file
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Convert to unix format
 vim.keymap.set("n", "<leader>U", "<cmd>!dos2unix %<CR>", { silent = true })
