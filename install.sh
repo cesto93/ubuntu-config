53ec00f326437b7a7765fc32f3f1297155b80050
Pier Francesco Contino <pfcontino@gmail.com>
2024-12-29 14:09:37 +0100

    feat(install): add plantuml

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sudo snap install nvim
+sudo snap install nvim go --classic
 
-sudo apt install fzf
+sudo apt install fzf textlive-full
+
+# goplantuml
+sudo go install go install github.com/jfeliu007/goplantuml/cmd/goplantuml@latest
+
 mkdir -p ~/.config/tmux/
 cp -r tmux/* ~/.config/tmux/
