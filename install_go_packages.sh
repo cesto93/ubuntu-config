f89fc3eb9e2be036ec6c6f07ea87b55798973221
Pier Francesco Contino <pier.contino@corteconti.it>
2026-04-29 11:28:28 +0200

    refactor

--- install_go_packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-# goplantuml
-sudo go install github.com/jfeliu007/goplantuml/cmd/goplantuml@latest
--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -2,3 +2,5 @@
 
 echo "Installing go via snap"
 sudo snap install go --classic
+
+sudo go install github.com/jfeliu007/goplantuml/cmd/goplantuml@latest
